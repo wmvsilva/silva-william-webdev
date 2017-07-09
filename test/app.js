50f671b9b28f3274c328eacb91fa69b169fe6b1a
William Silva <william@Williams-MacBook-Pro.local>
2017-07-08 21:38:43 -0400

    Change mongoDB URL

--- test/app.js
+++ test/app.js
@@ -10,7 +10,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds151062.mlab.com:51062/heroku_fjz486h2'; // user yours
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
