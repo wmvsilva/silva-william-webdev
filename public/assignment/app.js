58da0841381fc72adfce6033c705ae233f45ede0
William Silva <silva.w@husky.neu.edu>
2017-07-22 12:45:23 -0400

    Finish 3.6 lecture

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,5 +3,20 @@ var app = angular.module("WamApp", []);
 app.controller("loginController", loginController);
 
 function loginController($scope) {
-    $scope.hello = "hello from loginController";
+    // JSON = JavaScript Object Notation
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    $scope.login = function(user) {
+        for (var u in users) {
+            var _user = users[u];
+            if (_user.username === user.username && _user.password === user.password) {
+                $scope.welcomeUser = _user;
+            }
+        }
+    }
 }
