e19168b4afa35b94ce99fd2bf931681e30756d6e
William Silva <silva.w@husky.neu.edu>
2017-07-22 16:07:08 -0400

    Finish 3.12 lecture

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,47 +1,49 @@
-var app = angular.module("WamApp", ["ngRoute"]);
+(function() {
+    var app = angular.module("WamApp", ["ngRoute"]);
 
-app.controller("loginController", loginController);
-app.controller("profileController", profileController);
+    app.controller("loginController", loginController);
+    app.controller("profileController", profileController);
 
-app.config(configuration);
+    app.config(configuration);
 
-function configuration($routeProvider) {
-    $routeProvider
-        .when("/login", {
-            templateUrl: "login.html"
-        })
-        .when("/register", {
-            templateUrl: "register.html"
-        })
-        .when("/profile/:userId", {
-            templateUrl: "profile.html"
-        });
-}
-
-var users = [
-    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-];
-
-function profileController($scope, $routeParams) {
-    var userId = $routeParams["userId"];
-    for (var u in users) {
-        if (users[u]._id === userId) {
-            $scope.user = users[u];
-        }
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "login.html"
+            })
+            .when("/register", {
+                templateUrl: "register.html"
+            })
+            .when("/profile/:userId", {
+                templateUrl: "profile.html"
+            });
     }
-}
 
-function loginController($scope, $location) {
-    $scope.login = function(user) {
+    var users = [
+        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
+        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
+        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
+    ];
+
+    function profileController($scope, $routeParams) {
+        var userId = $routeParams["userId"];
         for (var u in users) {
-            var _user = users[u];
-            if (_user.username === user.username && _user.password === user.password) {
-                $location.url("profile/" + _user._id);
+            if (users[u]._id === userId) {
+                $scope.user = users[u];
             }
         }
-        $scope.errorMessage = "User not found";
     }
-}
+
+    function loginController($scope, $location) {
+        $scope.login = function (user) {
+            for (var u in users) {
+                var _user = users[u];
+                if (_user.username === user.username && _user.password === user.password) {
+                    $location.url("profile/" + _user._id);
+                }
+            }
+            $scope.errorMessage = "User not found";
+        }
+    }
+})();
