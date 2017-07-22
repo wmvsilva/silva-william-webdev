7f01c9d871c19f27ea3fe0896296c9035eb24a35
William Silva <silva.w@husky.neu.edu>
2017-07-22 13:38:49 -0400

    Finish 3.10 lecture

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,6 +1,7 @@
 var app = angular.module("WamApp", ["ngRoute"]);
 
 app.controller("loginController", loginController);
+app.controller("profileController", profileController);
 
 app.config(configuration);
 
@@ -12,25 +13,33 @@ function configuration($routeProvider) {
         .when("/register", {
             templateUrl: "register.html"
         })
-        .when("/profile", {
+        .when("/profile/:userId", {
             templateUrl: "profile.html"
         });
 }
 
-function loginController($scope, $location) {
-    // JSON = JavaScript Object Notation
-    var users = [
-        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-    ];
+var users = [
+    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+];
 
+function profileController($scope, $routeParams) {
+    var userId = $routeParams["userId"];
+    for (var u in users) {
+        if (users[u]._id === userId) {
+            $scope.user = users[u];
+        }
+    }
+}
+
+function loginController($scope, $location) {
     $scope.login = function(user) {
         for (var u in users) {
             var _user = users[u];
             if (_user.username === user.username && _user.password === user.password) {
-                $location.url("profile");
+                $location.url("profile/" + _user._id);
             }
         }
         $scope.errorMessage = "User not found";
