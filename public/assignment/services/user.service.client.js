79ca875d1cf826af7a0b24113e0f12e21beccf47
William Silva <silva.w@husky.neu.edu>
2017-07-25 14:51:43 -0400

    Piazza fixes

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -6,10 +6,14 @@
     function UserService() {
 
         var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
+            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder",
+                "email": "alice@wonder.com"},
+            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley",
+                "email": "bob@marley.com"},
+            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia",
+                "email": "charly@garcia.com"},
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi",
+                "email": "jose@annunzi.com"}
         ];
 
         var api = {
