6ecc3fce707ad296bc3dd8cb584c5a75fc1363a4
William Silva <silva.w@husky.neu.edu>
2017-07-28 12:39:48 -0400

    Finish 4.9 lecture

---
+++ assignment/app.js
@@ -0,0 +1,20 @@
+
+
+var users = [
+    {
+        _id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder",
+        "email": "alice@wonder.com"
+    },
+    {
+        _id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley",
+        "email": "bob@marley.com"
+    },
+    {
+        _id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia",
+        "email": "charly@garcia.com"
+    },
+    {
+        _id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi",
+        "email": "jose@annunzi.com"
+    }
+];
