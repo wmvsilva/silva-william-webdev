ebbd9db837081aaeea97f98f8931d85deb7aa1a2
William Silva <silva.w@husky.neu.edu>
2017-08-04 10:39:41 -0400

    Finish 5.4 lecture

--- public/project_poc/app.js
+++ public/project_poc/app.js
@@ -1,9 +1,10 @@
 (function () {
     angular
         .module("tmdbApp", [])
-        .controller("searchController", searchController);
+        .controller("searchController", searchController)
+        .service("movieService", movieService);
 
-    function searchController() {
+    function searchController(movieService) {
         var model = this;
 
         model.searchMovieByTitle = searchMovieByTitle;
@@ -14,7 +15,25 @@
         init();
 
         function searchMovieByTitle(movieTitle) {
-            alert(movieTitle);
+            movieService
+                .searchMovieByTitle(movieTitle)
+                .then(renderMovies);
+        }
+
+        function renderMovies(movies) {
+            model.movies = movies;
+        }
+    }
+
+    function movieService($http) {
+        this.searchMovieByTitle = searchMovieByTitle;
+
+        function searchMovieByTitle(movieTitle) {
+            var url = "https://api.themoviedb.org/3/search/movie?api_key=9a1db3dd9659485ffb9d482a484908e0&language=en-US&query="+movieTitle+"&page=1";
+            return $http.get(url)
+                .then(function (response) {
+                   return response.data;
+                });
         }
     }
 })();
