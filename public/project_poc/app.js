85e2bb3f072d1138f450929a8c545a8581a14515
William Silva <silva.w@husky.neu.edu>
2017-08-04 12:18:52 -0400

    Finish 5.6 + 5.7 lectures

--- public/project_poc/app.js
+++ public/project_poc/app.js
@@ -1,8 +1,24 @@
 (function () {
     angular
-        .module("tmdbApp", [])
+        .module("tmdbApp", ["ngRoute"])
+        .config(configuration)
         .controller("searchController", searchController)
+        .controller("detailsController", detailsController)
         .service("movieService", movieService);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "search.html",
+                controller: "searchController",
+                controllerAs: "model"
+            })
+            .when("/details/:id", {
+               templateUrl: "details.html",
+                controller: "detailsController",
+                controllerAs: "model"
+            });
+    }
 
     function searchController(movieService) {
         var model = this;
@@ -25,14 +41,40 @@
         }
     }
 
+    function detailsController($routeParams, movieService) {
+        var model = this;
+
+        model.id = $routeParams.id;
+
+        function init() {
+            movieService
+                .searchMovieById(model.id)
+                .then(renderMovie);
+        }
+        init();
+
+        function renderMovie(movie) {
+            model.movie = movie;
+        }
+    }
+
     function movieService($http) {
         this.searchMovieByTitle = searchMovieByTitle;
+        this.searchMovieById = searchMovieById;
 
         function searchMovieByTitle(movieTitle) {
             var url = "https://api.themoviedb.org/3/search/movie?api_key=9a1db3dd9659485ffb9d482a484908e0&language=en-US&query="+movieTitle+"&page=1";
             return $http.get(url)
                 .then(function (response) {
                    return response.data;
+                });
+        }
+
+        function searchMovieById(id) {
+            var url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=9a1db3dd9659485ffb9d482a484908e0";
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
                 });
         }
     }
