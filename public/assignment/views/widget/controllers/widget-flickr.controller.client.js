fcb4ea1ed8cfcf65617cf2ff336b5843799aa9e7
William Silva <silva.w@husky.neu.edu>
2017-08-09 10:33:26 -0400

    Make Flickr API look good

--- public/assignment/views/widget/controllers/widget-flickr.controller.client.js
+++ public/assignment/views/widget/controllers/widget-flickr.controller.client.js
@@ -3,14 +3,33 @@
         .module("WebAppMaker")
         .controller("flickrController", flickrController);
 
-    function flickrController(flickrService) {
+    function flickrController(flickrService, $routeParams, WidgetService, $location) {
         var model = this;
 
         model.searchPhotos = searchPhotos;
         model.selectPhoto = selectPhoto;
 
+        function init() {
+            model.userId = $routeParams["uid"];
+            model.websiteId = $routeParams["wid"];
+            model.pageId = $routeParams["pid"];
+            model.widgetId = $routeParams["wgid"];
+            WidgetService
+                .findWidgetById(model.widgetId)
+                .then(function (response) {
+                    model.widget = response.data;
+                });
+        }
+        init();
+
         function selectPhoto(photo) {
-            console.log(photo);
+            var url = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_m.jpg";
+            model.widget.url = url;
+            WidgetService
+                .updateWidget(model.widgetId, model.widget)
+                .then(function () {
+                    $location.url("/user/" + model.userId + "/website/" + model.websiteId + "/page/" + model.pageId + "/widget/"+model.widget._id);
+                });
         }
 
         function searchPhotos(searchTerm) {
