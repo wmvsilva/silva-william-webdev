037a253ad56e2bdf18b16dc23bc11a6c5e2de732
William Silva <silva.w@husky.neu.edu>
2017-08-09 03:45:31 -0400

    Get to 42:50 in html video

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -75,6 +75,9 @@
                 controller: "EditWidgetController",
                 controllerAs: "model"
             })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid/html", {
+                templateUrl: "views/widget/templates/editors/widget-html-edit.view.client.html"
+            })
             .when('/user/:uid/website/:wid/page/:pid/widget/:wgid/search', {
                 templateUrl: "views/widget/templates/widget-flickr-search.view.client.html",
                 controller: "flickrController",
