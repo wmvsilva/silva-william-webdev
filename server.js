a0ce10439ca05338abff6a1327428ea6cd4a354f
William Silva <silva.w@husky.neu.edu>
2017-08-14 13:03:45 -0400

    Finish 6.17 lecture

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var app = require('./express');
 var express = app.express;
+
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
