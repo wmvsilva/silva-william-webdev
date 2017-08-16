eea47636b56e373996f8051aadfafbd4efa680f7
William Silva <silva.w@husky.neu.edu>
2017-08-16 12:18:24 -0400

    Finish up admin users

--- public/project_prototype/views/admin/controllers/admin-user.controller.client.js
+++ public/project_prototype/views/admin/controllers/admin-user.controller.client.js
@@ -9,6 +9,8 @@
 
         this.createUser = createUser;
         this.selectUser = selectUser;
+        this.deleteUser = deleteUser;
+        this.updateUser = updateUser;
 
         function init() {
             UserService
@@ -21,12 +23,46 @@
         init();
 
         function createUser(user) {
-            console.log(user);
+            UserService
+                .createUser(user)
+                .then(function (response) {
+                    UserService
+                        .getAllUsers()
+                        .then(function (response) {
+                            model.users = response.data;
+                        });
+                })
         }
 
         function selectUser(user) {
             model.selectedUserId = user._id;
-            model.selectedUser = user;
+            model.selectedUser = angular.copy(user);
+        }
+
+        function deleteUser(userId) {
+            UserService
+                .deleteUser(userId)
+                .then(function (response) {
+                    UserService
+                        .getAllUsers()
+                        .then(function (response) {
+                            model.users = response.data;
+                        });
+                })
+        }
+
+        function updateUser(userId, user) {
+            UserService
+                .updateUser(userId, user)
+                .then(function (response) {
+                    UserService
+                        .getAllUsers()
+                        .then(function (response) {
+                            model.users = response.data;
+                        });
+                });
+            model.selectedUserId = null;
+            model.selectedUser = null;
         }
 
     }
